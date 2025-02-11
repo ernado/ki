b348c81cbd30134029885fb4b790324547dc58bc
Aleksandr Razumov <ernado@go-faster.org>
2025-02-11 17:21:23 +0300

    feat: configure kernel params

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"net"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"sort"
 
 	"github.com/go-faster/errors"
 )
@@ -83,15 +85,82 @@ func CheckTCPPortIsFree(n int) error {
 	return errors.Errorf("port %d is in use", n)
 }
 
+func LoadKernelModules(name string, modules ...string) error {
+	for _, module := range modules {
+		fmt.Println("> Loading module", module)
+		cmd := exec.Command("modprobe", module)
+		cmd.Stderr = os.Stderr
+		cmd.Stdout = os.Stdout
+		if err := cmd.Run(); err != nil {
+			return errors.Wrapf(err, "modprobe %s", module)
+		}
+	}
+	// Persist in named configuration.
+	fileName := filepath.Join("/etc/modules-load.d/", name+".conf")
+	fmt.Printf("> Writing %s\n", fileName)
+	var out []byte
+	for _, module := range modules {
+		out = append(out, module...)
+		out = append(out, '\n')
+	}
+	if err := os.WriteFile(fileName, out, 0644); err != nil {
+		return errors.Wrap(err, "write")
+	}
+	return nil
+}
+
+func ConfigureKernelParameters(name string, params map[string]any) error {
+	fmt.Printf("> Configuring kernel parameters for %s\n", name)
+	fileName := filepath.Join("/etc/sysctl.d/", name+".conf")
+	keys := make([]string, 0, len(params))
+	for key := range params {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	fmt.Printf("> Writing %s\n", fileName)
+	var out []byte
+	for _, key := range keys {
+		out = append(out, key...)
+		out = append(out, '=')
+		out = append(out, fmt.Sprintf("%v", params[key])...)
+		out = append(out, '\n')
+	}
+	if err := os.WriteFile(fileName, out, 0644); err != nil {
+		return errors.Wrap(err, "write")
+	}
+	// Reload.
+	cmd := exec.Command("sysctl", "--system")
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	if err := cmd.Run(); err != nil {
+		return errors.Wrap(err, "sysctl --system")
+	}
+	return nil
+}
+
 func run() error {
+	// 1. Check required ports
+	if err := CheckTCPPortIsFree(6443); err != nil {
+		return errors.Wrap(err, "check k8s port")
+	}
+	// 2. Swap configuration
 	if err := DisableSwap(); err != nil {
 		return errors.Wrap(err, "disable swap")
 	}
+	// 3. Update apt cache
 	if err := APTUpdate(); err != nil {
 		return errors.Wrap(err, "apt update")
 	}
-	if err := CheckTCPPortIsFree(6443); err != nil {
-		return errors.Wrap(err, "check k8s port")
+	// Installing a container runtime.
+	if err := LoadKernelModules("containerd", "overlay", "br_netfilter"); err != nil {
+		return errors.Wrap(err, "load kernel modules")
+	}
+	if err := ConfigureKernelParameters("kubernetes", map[string]any{
+		"net.bridge.bridge-nf-call-ip6tables": 1,
+		"net.bridge.bridge-nf-call-iptables":  1,
+		"net.ipv4.ip_forward":                 1,
+	}); err != nil {
+		return errors.Wrap(err, "configure kernel parameters")
 	}
 	return nil
 }
