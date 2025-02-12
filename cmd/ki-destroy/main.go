ed31c30144fdf62a7b5d2157ca31ad0b115f5f02
Aleksandr Razumov <ernado@go-faster.org>
2025-02-12 11:34:53 +0300

    feat: add ki-destroy step to e2e workflow and enhance logging

--- cmd/ki-destroy/main.go
+++ cmd/ki-destroy/main.go
@@ -15,19 +15,20 @@ func run() error {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*5)
 	defer cancel()
 
-	// Remove all servers.
+	fmt.Println("> Removing all servers")
 	servers, err := client.Server.All(ctx)
 	if err != nil {
 		return errors.Wrap(err, "get servers")
 	}
 	for _, server := range servers {
+		fmt.Println("Removing server", server.Name)
 		res, _, err := client.Server.DeleteWithResult(ctx, server)
 		if err != nil {
 			return errors.Wrap(err, "delete server")
 		}
 		// Wait until action processed.
 		if err := client.Action.WaitForFunc(ctx, func(update *hcloud.Action) error {
-			fmt.Println("Action updated", update.Status)
+			fmt.Printf("Server(%s): %s\n", server.Name, update.Command)
 			return nil
 		}, res.Action); err != nil {
 			return errors.Wrap(err, "wait for action")
@@ -35,24 +36,26 @@ func run() error {
 		fmt.Println("Server removed")
 	}
 
-	// Remove all load balancers.
+	fmt.Println("> Removing all load balancers")
 	lbs, err := client.LoadBalancer.All(ctx)
 	if err != nil {
 		return errors.Wrap(err, "get load balancers")
 	}
 	for _, lb := range lbs {
+		fmt.Println("Removing load balancer", lb.Name)
 		if _, err := client.LoadBalancer.Delete(ctx, lb); err != nil {
 			return errors.Wrap(err, "delete load balancer")
 		}
 		fmt.Println("Load balancer removed")
 	}
 
-	// Remove all networks.
+	fmt.Println("> Removing all networks")
 	networks, err := client.Network.All(ctx)
 	if err != nil {
 		return errors.Wrap(err, "get networks")
 	}
 	for _, network := range networks {
+		fmt.Println("Removing network", network.Name)
 		if _, err := client.Network.Delete(ctx, network); err != nil {
 			return errors.Wrap(err, "delete network")
 		}
