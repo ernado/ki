925bb9c194cb1eaf682082e87df09d7a797eb424
Aleksandr Razumov <ernado@go-faster.org>
2025-02-12 07:30:42 +0300

    fix(httpserver): set timeouts

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -1,13 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"time"
+)
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		_, _ = w.Write([]byte("Hello, World!"))
 	})
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	srv := &http.Server{
+		Addr:              ":8080",
+		Handler:           http.DefaultServeMux,
+		ReadTimeout:       time.Second,
+		ReadHeaderTimeout: time.Second,
+	}
+	if err := srv.ListenAndServe(); err != nil {
 		panic(err)
 	}
 }
